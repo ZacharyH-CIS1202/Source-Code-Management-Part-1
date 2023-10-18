02528d43fd27d9a3b91a70a5c392630689c63369
Zach <Zach@ZACHS-PC>
2023-10-17 20:38:53 -0400

    Add project files.

---
+++ Source Code Management Part 1/Hello_World_Original.cpp
@@ -0,0 +1,17 @@
+/*
+Zachary Hartings
+10/17/2023
+CIS 1202.201
+Source Code Mgmt Part 1: Display Hello World
+*/
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+
+	return 0;
+}
